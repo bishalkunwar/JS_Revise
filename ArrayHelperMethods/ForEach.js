a3ff391875ceac54955f6f85da0e289953c03af8
Bishal487 <bishal.kunwar487@gmail.com>
2024-02-25 19:47:20 -0500

    forEach scenario in not known length of array case

--- ArrayHelperMethods/ForEach.js
+++ ArrayHelperMethods/ForEach.js
@@ -13,4 +13,17 @@ colors.forEach(function(color){
 });
 
 
+// ForEach scenario
+let numbers = [1,2,3,4,5,6]
+let sum = 0;
+function adder(number){
+    sum+=number;
+};
+
+numbers.forEach(adder);
+console.log(sum);
+// loop over the array, incrementing the sum variable.
+
+
+
 // For in VS for of and then for each
