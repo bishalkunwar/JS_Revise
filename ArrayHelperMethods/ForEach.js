e7277087713cd5f706c6146292c6e903d9dc91d7
Bishal487 <bishal.kunwar487@gmail.com>
2024-02-25 19:35:41 -0500

    Revising JS

---
+++ ArrayHelperMethods/ForEach.js
@@ -0,0 +1,16 @@
+
+// For loop
+var colors = ['red', 'blue', 'green'];
+
+for(var i=0; i<colors.length; i++){
+    console.log(colors[i]);
+};
+
+
+// for each 
+colors.forEach(function(color){
+    console.log(color);
+});
+
+
+// For in VS for of and then for each
