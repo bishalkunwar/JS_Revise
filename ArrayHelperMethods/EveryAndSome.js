57595f04162761488c4f75afb477afe46e628f5b
Bishal487 <bishal.kunwar487@gmail.com>
2024-02-27 17:47:47 -0500

    real case scenario of every and some array method

--- ArrayHelperMethods/EveryAndSome.js
+++ ArrayHelperMethods/EveryAndSome.js
@@ -28,3 +28,36 @@ const zeroComp = computers.every(function(computer){
 });
 
 console.log(zeroComp);
+
+
+const heroComp = computers.some(function(computer){
+    return computer.ram <= 28;
+});
+
+console.log(heroComp);
+
+
+// Real case scenario of every and some.
+function Field(value){
+    this.value = value;
+};
+
+Field.prototype.validate = function(){
+    return this.value.length >0;
+};
+
+const userName= new Field("Bishal");
+const password = new Field("A123");
+
+// userName.validate() && password.validate();
+let fields = [userName, password];
+
+let isValid = fields.every(function(field){
+    return field.validate();
+});
+
+if(isValid){
+    console.log("let the user login");
+}else{
+    console.log("Garbage user");
+}
