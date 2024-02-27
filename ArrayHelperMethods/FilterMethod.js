13720384ebd121baeea3794efda7af5cf99cb249
Bishal487 <bishal.kunwar487@gmail.com>
2024-02-26 21:51:56 -0500

    curry function returning filter method

--- ArrayHelperMethods/FilterMethod.js
+++ ArrayHelperMethods/FilterMethod.js
@@ -16,3 +16,24 @@ for(let x=0; x<products.length;x++){
 
 console.log(filteredFruits);
 
+
+// Filter method: ()=>
+const newFruits = products.filter(function(product){
+    return product.type === "fruit" && product.name === "apple";
+});
+
+console.log(newFruits);
+
+
+// function curry and filter method example:=>
+const posts = {id: 2, title: "New Post"};
+const comments = [{postId: 3, content: "awesome"}, {postId: 2, content: "Neat"}];
+
+function commentsOfThePost(posts, comments){
+    return comments.filter(function(comment){
+        return comment.postId === posts.id;
+    });
+};
+
+console.log(commentsOfThePost(posts, comments));
+
