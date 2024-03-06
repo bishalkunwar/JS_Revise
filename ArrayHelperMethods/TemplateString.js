0bd771a0aaf7cec3d222e2dbbf6a47e3a25c22de
Bishal487 <bishal.kunwar487@gmail.com>
2024-03-05 19:07:54 -0500

    template string use case

--- ArrayHelperMethods/TemplateString.js
+++ ArrayHelperMethods/TemplateString.js
@@ -15,3 +15,17 @@ const dateToday = ()=>{
 } 
 
 console.log(dateToday());
+
+
+// Use Case scenario of template strings-.
+const namee = "Bishal";
+const age = 12;
+let address = "41 Sheppard Village";
+
+// normal console
+console.log("My Name is "+namee+" I am "+age+"years old"+"I live in "+address);
+
+// ES 6
+console.log(`My Name is ${namee} I am ${age} years old, I live in ${address}`);
+
+// So basically, in complex scenario template string makes printing easy as well as less chances of error.
